d6ea1c5a48035adf7f5c2ef99e4dd1220d3ca5e4
ayu.purnamaningsih <ayu.purnamaningsih@brank.as>
2024-11-08 23:30:27 +0800

    my first test

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,30 @@
 const { defineConfig } = require("cypress");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
   video: false,
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 10000,
+  numTestsKeptInMemory: 0,
   e2e: {
-    baseUrl: "https://www.saucedemo.com/",
+    experimentalSessionAndOrigin: true,
+    defaultCommandTimeout: 60000,
     excludeSpecPattern: ["**/1-getting-started", "**/2-advanced-examples"],
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+  },
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
+    },
+  },
+  compilerOptions: {
+    types: ["cypress", "cypress-file-upload"],
   },
 });
